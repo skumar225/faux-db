1722561e7ea9f76fe4ed2fe83127fd7289b29194
Shilpa Kumar <skumar225@gmail.com>
2015-02-12 09:26:48 -0800

    Initial commit

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'pry'
 require 'sinatra'
-require 'sinatra/reloader'
+# require 'sinatra/reloader'
 require "better_errors"
 
 configure :development do
@@ -26,10 +26,38 @@ end
 
 # GET /users/new - display a form for making a new user
 #
+get '/users/new' do
+  erb :new
+end
 # POST /users - create a user based on params from form
-#
+post '/users' do
+  p params
+  @users = users
+  users.push({first: params[:first], last: params[:last], id: id})
+  id+= 1
+  redirect '/'
+end
 # GET /users/:id - show a user's info by their id, this should display the info in a form
-#
+
+get '/users/:id' do
+  #loop through array, find where id params equals id and 
+  @user = users[params[:id].to_i - 1]
+  erb :show
+end
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
-#
+
+put '/users/:id' do 
+  users[:first] = users[params[:id].to_i - 1]  #identifies which name and converts ID to index position
+
+  users[:first] = params[:first] #updates info
+  users[:last] = params[:last]
+end 
+
 # DELETE /users/:id - delete a user by their id
+
+users.each do |user| 
+     if user[:id] == params[:id].to_i
+      delete.user
+       # now we found the right user
+     end
+   end
